cc779f969463042853247a69f433b30876d3b476
franlops <franlopbri@gmail.com>
2022-10-18 13:57:59 -0500

    Asignacion dinamica de eventos a botones

--- app.js
+++ app.js
@@ -1,20 +1,48 @@
 const urlBase = 'https://rickandmortyapi.com/api/character/';
 
-fetch(urlBase)
-    .then(respuesta => {
-        //console.log(respuesta);
-        return respuesta.json();
-    })
+const loadData = (url, page = 1) => {
+    url += `?page=${page}`
+    fetch(url)
+    .then(respuesta => respuesta.json())
     .then(respJson => {
         const info = respJson.info;
         const personajes = respJson.results;
-        console.log(info);
+        console.log(info.next);
+        console.log(info.prev);
+        if(!info.prev){
+            document.querySelector('#prev').classList.add('disabled')
+        } else {
+            document.querySelector('#prev').classList.remove('disabled')
+            document.querySelector('#prev').setAttribute('data-id', page - 1)
+        }
+        if(!info.next){
+            document.querySelector('#next').classList.add('disabled')
+        } else {
+            document.querySelector('#next').classList.remove('disabled')
+            document.querySelector('#next').setAttribute('data-id', page + 1)
+        }
         console.log(personajes);
         showCharacters(personajes);
     })
+}
+
+const navegacion = (e) => {
+    e.preventDefault();
+    if(e.target.classList.contains('btn')){
+        let page = e.target.getAttribute('data-id');
+        loadData(urlBase, page);
+    }
+}
+
+loadData(urlBase);
+
+document.querySelector('.botones').addEventListener('click', navegacion);
 
 const showCharacters = (personajes) => {
     const contenedorRespuesta = document.querySelector('#respuesta');
+    while(contenedorRespuesta.firstChild){
+        contenedorRespuesta.remove(contenedorRespuesta.firstChild);
+    }
     personajes.forEach(personaje => {
         contenedorRespuesta.appendChild(creaCard(personaje));
     })
